1f612cea91840115628d6ef19d83c27d3905cd7f
Yurkin Alexandr <yurkin1976@gmail.com>
2022-04-16 20:07:39 +0300

    add Dz1,Dz3

---
+++ DZ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите два числа ");
+//int number = A;
+int numberA = Convert.ToInt32(Console.ReadLine());
+//int number = B;
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+
+if  (max  > numberB)
+{
+    Console.Write("Большее число" + numberA);
+}
+else
+{
+    Console.Write("Большее число" + numberB);
+}
