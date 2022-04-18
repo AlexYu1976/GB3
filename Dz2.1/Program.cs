9804c02eab78b0790211cee5e5770bf30c8ac536
Yurkin Alexandr <yurkin1976@gmail.com>
2022-04-18 14:57:33 +0300

    add Dz2.1

---
+++ Dz2.1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа которая выводит случайное число и удаляет вторую цифру
+
+int number = RandomInt(100, 1000);
+Console.WriteLine(number);
+string stringNumber = Convert.Tostring(number);
+Console.WriteLine($"{stringNumder[0]}{stringNumber[2]}");
+
+int RandomInt(int anclusiveMin,int axlusiveMax)
+{
+    return new Random().Next(anclusiveMin,axlusiveMax);
+}
