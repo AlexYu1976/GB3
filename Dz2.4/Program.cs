6e6b389b0d08761418c2f7cea40935cfee6844a6
Yurkin Alexandr <yurkin1976@gmail.com>
2022-04-18 18:02:58 +0300

    add Dz2.4

---
+++ Dz2.4/Program.cs
@@ -0,0 +1,57 @@
+﻿// Программа которая принимает на вход цифру, обозначающую
+// день недели и проверяет является ли этот день выходным.
+
+Console.WriteLine("Ведите цифру от 1 до 7 ");
+int day = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Week(day) + "-" + CheckHollyday(Week(day)));
+
+
+string Week(int day)
+{
+
+    if (day==1)
+    {
+        stringDay = ConvertToString(day);
+        stringDay = "Пн";
+        return stringDay;
+    }
+    if (day==2)
+    {
+        stringDay = ConvertToString(day);
+        stringDay = "Вт";
+        return stringDay; 
+    }
+
+    if (day==3)
+    {
+        stringDay = ConvertToString(day);
+        stringDay = "Ср";
+        return stringDay; 
+
+    }
+        if (day==4)
+    {
+        stringDay = ConvertToString(day);
+        stringDay = "Чт";
+        return stringDay; 
+    }
+        if (day==5)
+    {
+        stringDay = ConvertToString(day);
+        stringDay = "Пт";
+        return stringDay; 
+    }
+        if (day==6)
+    {
+        stringDay = ConvertToString(day);
+        stringDay = "Сб";
+        return stringDay; 
+    }   
+        if (day==7)
+    {
+        stringDay = ConvertToString(day);
+        stringDay = "Вс";
+        return stringDay; 
+    }
+}
+
