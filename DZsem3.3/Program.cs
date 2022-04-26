c601a12219c05f429b2204a317f861c1f596582d
Yurkin Alexandr <yurkin1976@gmail.com>
2022-04-26 23:45:45 +0300

    добавили решение задач в папки  DZsem3.1-DZsem3.3

--- DZsem3.3/Program.cs
+++ DZsem3.3/Program.cs
@@ -2,3 +2,64 @@
 //Задайте массив вещественных чисел.
 // Найдите разницу между максимальным и минимальным элементов массива.
 //[3 7 22 2 78] -> 76
+
+Console.WriteLine("Введите длину массива ");
+int Length = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[Length];
+FillArray(numbers);
+PrintArray(numbers);
+Max(numbers);
+Console.WriteLine("Максимальный элемент массива " + Max(numbers));
+Min(numbers);
+Console.WriteLine("Минимальный элемент массива " + Min(numbers));
+double result = Max(numbers) - Min(numbers);
+Console.WriteLine("Разница между максимальным и минимальным элементом маасива = " +result);
+
+void FillArray(double[] array) // Задали массив
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+}
+
+
+void PrintArray(double[] array1) // Вывели массив на экран
+{
+    for(int i = 0; i < array1.Length; i++)
+    {
+        Console.Write(array1[i]+ " ");
+       
+    }
+    Console.WriteLine();
+}
+
+double Max(double[] numbers) // Метод нахождения максимального элемента массива
+{
+    double maxValue = numbers[0];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > maxValue)
+        {
+            maxValue = numbers[i];
+        }
+    }
+    return maxValue;
+}
+
+double Min(double[] numbers) // Метод нахождения минимального элемента иассива
+{
+    double minValue = numbers[0];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] < minValue)
+        {
+            minValue = numbers[i];
+        }
+    }
+    return minValue;
+}
+
+
+
+
